bd119ab9221891d38d7d6f2d521edbee780bc9df
Matthew Lau <matthewlau@Matthews-MacBook-Pro.local>
2019-12-25 17:01:11 +0800

    updated backend

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(express.json({ extended: false }));
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/profile", require("./routes/api/profile"));
-app.use("/api/posts", require("./routes/api/posts"));
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
